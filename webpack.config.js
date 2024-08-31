d9d554f83296cde57394aec44b41f9b026cc39a2
Tran Minh Phu <trminhphu79@gmail.com>
2024-08-31 21:56:24 +0700

    update prefix chunk file after compile

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.[contenthash].js',
+        filename: 'chunk.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
